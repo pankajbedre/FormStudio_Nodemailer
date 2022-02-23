a266055196fb0204c1e1ae52b9091987ec80cbac
Pankaj Bedre <pankajbedre22@gmail.com>
2022-02-24 01:03:16 +0530

    html added

--- index.js
+++ index.js
@@ -17,9 +17,17 @@ var sender = nodemailer.createTransport({
 var mail = {
     from: "pankajbedre06@gmail.com",
     to: "pankajbedre22@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy! 12345",
-    html: "<h1>Hello</h1>"
+    subject: "You have successfully submited the Form",
+    html: `<div style='display: flex; width: 100%; justify-content: center;'>
+    <div style='padding: 1.5em; display: inline-block; box-shadow: 2px 2px 10px #0f46461c; border-radius: .3em; margin-top: 2em;'>
+        <img width='150px' src='https://formstudio.io/form-studio-logo.svg' alt=''>
+
+        <h4 style='font-family:Verdana, Geneva, Tahoma, sans-serif; font-weight: 200'>Hey, <br> You have successfully submited the form!</h4>
+        <h3 style='font-family:Verdana, Geneva, Tahoma, sans-serif; font-weight: 300; margin-top: 3em;'>Easily Create Customizable <br> Forms, Quizs, Surveys and much more... </h3>
+
+        <a target='blank' href='https://formstudio.io' style='font-family:Verdana, Geneva, Tahoma, sans-serif; background-color: #06a0a0; color: #fff; border: none; padding: .7em 1.5em; font-size: 1.2em; border-radius: .2em; margin-top: .5em; display: inline-block; text-decoration: none;'>Go To FormStudio</a>
+        </div>
+    </div>`
 };
 
 
